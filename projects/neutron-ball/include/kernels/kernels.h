1cc1a07d827c6a0dbf7a92bf2ba71361d1c05d4d
Valmor F. de Almeida <dealmeidavf@gmail.com>
2021-03-28 16:00:05 -0500

    Bug fix on header

--- projects/neutron-ball/include/kernels/kernels.h
+++ projects/neutron-ball/include/kernels/kernels.h
@@ -11,13 +11,13 @@
 
 #include "MooseApp.h"
 
-class Engy5310P1App : public MooseApp
+class NeutronBallApp : public MooseApp
 {
 public:
   static InputParameters validParams();
 
-  Engy5310P1App(InputParameters parameters);
-  virtual ~Engy5310P1App();
+  NeutronBallApp(InputParameters parameters);
+  virtual ~NeutronBallApp();
 
   static void registerApps();
   static void registerAll(Factory & f, ActionFactory & af, Syntax & s);
