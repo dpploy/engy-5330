31012fb394acda8aa111c8dea0d1adb6483aee37
Valmor F. de Almeida <dealmeidavf@gmail.com>
2021-05-02 17:37:50 -0500

    Interface coupling

--- moose-app-templates/ScalarVariableIntegralPostprocessor.C
+++ moose-app-templates/ScalarVariableIntegralPostprocessor.C
@@ -7,6 +7,8 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 //
+// Engy-5310: Computational Continuum Transport Phenomena
+// UMass Lowell, Nuclear Chemical Engineering
 // https://github.com/dpploy/engy-5310
 
 // Template includes
